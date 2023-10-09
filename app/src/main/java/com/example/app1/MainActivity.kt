7f6be57e42b07251d7f58132a61373e0ba198054
victorluiz3315 <140361500+victorluiz3315@users.noreply.github.com>
2023-10-09 00:16:12 -0300

    Add files via upload

--- app/src/main/java/com/example/app1/MainActivity.kt
+++ app/src/main/java/com/example/app1/MainActivity.kt
@@ -8,6 +8,8 @@ import android.view.View
 import com.example.app1.databinding.ActivityMainBinding
 import com.example.app1.view.recover
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.Snackbar.SnackbarLayout
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
